865be8f133116c1443e27772e48e10bf353da2cc
ElMariuso <thiry.marius@gmail.com>
2022-07-17 13:59:06 +0200

    De-comment

--- srcs/token_simplification.c
+++ srcs/token_simplification.c
@@ -128,8 +128,8 @@ int token_simplification(t_input *data)
         return (1);
     if (word_total_fusion(elem, data) == 1)
         return (1);
-    // if (word_quote_fusion(elem, data) == 1)
-    //     return (1);
+    if (word_quote_fusion(elem, data) == 1)
+        return (1);
     if (delete_useless_wspace(elem, data) == 1)
         return (1);
     ms_token_print(data->args);
