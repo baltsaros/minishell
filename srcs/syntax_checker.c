cf1d6b4d87f50f527f277d71223efeb31f2df63d
ElMariuso <thiry.marius@gmail.com>
2022-07-18 23:55:02 +0200

    Update

--- srcs/syntax_checker.c
+++ srcs/syntax_checker.c
@@ -2,20 +2,14 @@
 
 int	is_the_next_is_in_arg(t_node *args)
 {
-	t_node	*elem;
-
-	elem = args;
-	if (elem->next->type != IN_ARG)
+	if (args->next->type != IN_ARG)
 		return (1);
 	return (0);
 }
 
 int	is_the_next_is_out_arg(t_node *args)
 {
-	t_node	*elem;
-
-	elem = args;
-	if (elem->next->type != OUT_ARG)
+	if (args->next->type != OUT_ARG)
 		return (1);
 	return (0);
 }
@@ -24,7 +18,9 @@ int	is_the_next_is_right_type(t_node	*args)
 {
 	if (args->next->type != WORD && args->next->type != ASTER
 		&& args->next->type != ENV_VA && args->next->type != ENV_VA_BR 
-		&& args->next->type != ENV_P && args->next->type != IN_ARG && args->next->type != OUT_ARG && args->next->type != EXECUTABLE)
+		&& args->next->type != ENV_P && args->next->type != IN_ARG 
+		&& args->next->type != OUT_ARG && args->next->type != EXECUTABLE
+		&& args->next->type != ENV_BR_EM && args->next->type != ENV_P_EM)
 		return (1);
 	return (0);
 }
