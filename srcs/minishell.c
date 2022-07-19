a2fe1b6eb78b4d170b6289346c09170c6853b45f
baltsaros <buzdin.alexander@gmail.com>
2022-07-19 17:29:43 +0200

    upd for prompt

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -18,15 +18,8 @@ void	prompt(t_input *data)
 			// ms_token_print(data->args);
 			// ms_envp_print(data->envp_n);
 			token_simplification(data);
-			if (!parsing(data))
-			{
-				asterisks(data, data->cmds);
-				execute(data);
-				//ms_free_token(data->args);
-				// ms_free_cmd(data->cmds);
-			}
-			// else
-				//ms_free_token(data->args);
+			execute(data);
+			// ms_free_token(data->args);
 		}
 		free(data->buf);
 	}
