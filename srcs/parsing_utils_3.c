1e661b78063dc3aeab5e715b11308ec4840a6821
ElMariuso <thiry.marius@gmail.com>
2022-07-21 16:22:46 +0200

    Norme update

--- srcs/parsing_utils_3.c
+++ srcs/parsing_utils_3.c
@@ -28,3 +28,58 @@ char	**init_cmd(t_node *args, t_input *data)
 	str[i] = NULL;
 	return (str);
 }
+
+t_cmd	*init_empty_elem(t_input *data)
+{
+	t_cmd	*elem;
+
+	elem = ms_malloc(sizeof(t_cmd), data);
+	elem->cmd = NULL;
+	elem->len_cmd = 0;
+	elem->delim = NULL;
+	elem->in = 0;
+	elem->in_arg = NULL;
+	elem->out = 1;
+	elem->out_arg = NULL;
+	elem->pipe = -1;
+	elem->next = NULL;
+	elem->prev = NULL;
+	return (elem);
+}
+
+t_cmd	*fill_elem(t_node *args, t_cmd *elem, t_input *data)
+{
+	elem->cmd = init_cmd(args, data);
+	if (!elem->cmd)
+		return (NULL);
+	elem->len_cmd = get_len_cmd(elem->cmd);
+	while (args && args->type != PIPE)
+	{
+		if (redirection_check(args, elem, data) == 1)
+		{
+			ms_free_cmd(elem);
+			return (NULL);
+		}
+		if (args->next && args->next->type == PIPE)
+		{
+			if (!args->next->next || is_the_next_is_right_type(args->next) == 1)
+				return (print_syntax_error_cmd(args->next));
+			elem->pipe = 1;
+		}
+		args = args->next;
+	}
+	return (elem);
+}
+
+t_cmd	*init_elem(t_node *args, t_input *data)
+{
+	t_cmd	*elem;
+
+	elem = init_empty_elem(data);
+	if (!elem)
+		return (NULL);
+	elem = fill_elem(args, elem, data);
+	if (!elem)
+		return (NULL);
+	return (elem);
+}
