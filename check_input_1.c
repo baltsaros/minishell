0a2b87f778ce8c89c87df5cd995dc2043821fbb7
baltsaros <buzdin.alexander@gmail.com>
2022-08-08 18:42:58 +0200

    fix of |

--- check_input_1.c
+++ check_input_1.c
@@ -6,7 +6,7 @@
 /*   By: abuzdin <abuzdin@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 09:29:59 by abuzdin           #+#    #+#             */
-/*   Updated: 2022/08/08 12:16:12 by abuzdin          ###   ########.fr       */
+/*   Updated: 2022/08/08 18:42:25 by abuzdin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ static int	read_after(char **buf, char c, t_input *data)
 // syntax check for pipe
 static int	before_pipe(char *str, int i)
 {
-	if (!str[i])
+	if (!str[i] || str[i] == '|')
 	{
 		write(2, "YAMSP: ", 7);
 		write(2, "syntax error near unexpected token `|'\n", 39);
@@ -74,7 +74,7 @@ static int	before_pipe(char *str, int i)
 	}
 	while (check_charset(str[i], " \f\n\r\t\v"))
 		--i;
-	if (str[i] && check_charset(str[i], "><"))
+	if (str[i] && check_charset(str[i], "|><"))
 	{
 		write(2, "YAMSP: ", 7);
 		write(2, "syntax error near unexpected token `|'\n", 39);
